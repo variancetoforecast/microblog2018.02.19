c9a85bb5a09e153e70881964ffd9212e6bc45107
Dylan <dylan@test.com>
2018-02-19 21:35:54 +1100

    first commit

---
+++ app/routes.py
@@ -0,0 +1,44 @@
+from flask import render_template, flash, redirect, url_for
+from app import app
+from app.forms import LoginForm
+
+from flask_login import current_user, login_user
+from app.models import User
+
+from flask_login import logout_user
+
+@app.route('/')
+@app.route('/index')
+
+def index():
+    user = {'username': 'Dylan'}
+    posts = [
+        {
+            'author': {'username': 'Banda'},
+            'body': 'Test post 1 it\'s great'          
+        },
+        {
+            'author': {'username': 'Ryan'},
+            'body': 'Test post 1 it\'s great'          
+        }
+    ]
+    return render_template('index.html', title='home', user=user, posts = posts)
+
+@app.route('/login', methods=['GET','POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
